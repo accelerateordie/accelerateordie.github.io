bbd2c9bab87933e2086ddeba4551baae9d22fd2f
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-08-03 17:17:40 +0330

    binding footer & header & other improvements (#8)

--- app/copy/footer.ts
+++ app/copy/footer.ts
@@ -1,116 +1,114 @@
-export const footerCopy =  {
-        "social-links": {
-            "x": {
-                "url": "https://x.com/accel_co",
-                "target": "_blank"
-            },
-            "instagram": {
-                "url": "https://instagram.com/accel_co",
-                "target": "_blank"
-            },
-            "github": {
-                "url": "https://github.com/accelerateordie",
-                "target": "_blank"
-            },
-            "discord": {
-                "url": "https://discord.gg/CEW5Gp9BGz",
-                "target": "_blank"
-            },
-            "linkedin": {
-                "url": "https://www.linkedin.com/company/accel-co/",
-                "target": "_blank"
-            },
-            "youtube": {
-                "url": "https://www.youtube.com/@accel_company",
-                "target": "_blank"
-            },
-            "tiktok": {
-                "url": "https://tiktok.com/accel_co",
-                "target": "_blank"
-            }
-        },
-        "quick-links": [
-            {
-                "name": "Home",
-                "url": "/",
-                "target": "_self"
-            },
-            {
-                "name": "How to Accelerate",
-                "url": "/how-to-accelerate",
-                "target": "_self"
-            },
-            {
-                "name": "School of ACCEL",
-                "url": "/school-of-accel",
-                "target": "_self"
-            },
-            {
-                "name": "My Journey",
-                "url": "/my-journey",
-                "target": "_self"
-            },
-            {
-                "name": "ACCEL Tunnel",
-                "url": "https://accel.io",
-                "target": "_blank"
-            }
-        ],
-        "quick-links-mob": [
-            {
-                "name": "Home",
-                "url": "/index-mobile",
-                "target": "_self"
-            },
-            {
-                "name": "How to Accelerate",
-                "url": "/how-to-accelerate-mobile",
-                "target": "_self"
-            },
-            {
-                "name": "School of ACCEL",
-                "url": "/school-of-accel-mobile",
-                "target": "_self"
-            },
-            {
-                "name": "My Journey",
-                "url": "/my-journey-mobile",
-                "target": "_self"
-            },
-            {
-                "name": "ACCEL Tunnel",
-                "url": "https://accel.io",
-                "target": "_blank"
-            }
-        ],
-        "resources": [
-            {
-                "name": "Resources",
-                "url": "#",
-                "target": "_blank"
-            },
-            {
-                "name": "Media & Press",
-                "url": "#",
-                "target": "_blank"
-            },
-            {
-                "name": "FAQ",
-                "url": "#",
-                "target": "_blank"
-            }
-        ],
-        "legal": [
-            {
-                "name": "Terms of Service",
-                "url": "TERMS",
-                "target": "_blank"
-            },
-            {
-                "name": "Privacy Policy",
-                "url": "#",
-                "target": "_blank"
-            }
-        ],
-        "copyright": "© Copyright ACCELERATE OR DIE. All rights reserved"
-    }
+export const footerCopy = {
+  socialLinks: [
+    {
+      name: "x",
+      image: "./img/icon-x.svg",
+      url: "https://x.com/accel_co",
+      target: "_blank",
+    },
+
+    {
+      name: "instagram",
+      image: "./img/icon-instagram.svg",
+      url: "https://instagram.com/accel_co",
+      target: "_blank",
+    },
+    {
+      name: "github",
+      image: "./img/icon-github.svg",
+      url: "https://github.com/accelerateordie",
+      target: "_blank",
+    },
+    {
+      name: "discord",
+      image: "./img/icon-discord.svg",
+      url: "https://discord.gg/CEW5Gp9BGz",
+      target: "_blank",
+    },
+    {
+      name: " linkedin",
+      image: "./img/icon-linkedin.svg",
+      url: "https://www.linkedin.com/company/accel-co/",
+      target: "_blank",
+    },
+    {
+      name: " youtube",
+      image: "./img/icon-youtube.png",
+      url: "https://www.youtube.com/@accel_company",
+      target: "_blank",
+    },
+    {
+      name: "tiktok",
+      image: "./img/icon-tiktok.svg",
+      url: "https://tiktok.com/accel_co",
+      target: "_blank",
+    },
+  ],
+  quickLinks: {
+    title: "Quick Links",
+    links: [
+      {
+        name: "Home",
+        url: "/",
+        target: "_self",
+      },
+      {
+        name: "How to Accelerate",
+        url: "/how-to-accelerate",
+        target: "_self",
+      },
+      {
+        name: "School of ACCEL",
+        url: "/school-of-accel",
+        target: "_self",
+      },
+      {
+        name: "My Journey",
+        url: "/my-journey",
+        target: "_self",
+      },
+      {
+        name: "ACCEL Tunnel",
+        url: "https://accel.io",
+        target: "_blank",
+      },
+    ],
+  },
+
+  resources: {
+    title: "Resources",
+    links: [
+      {
+        name: "Resources",
+        url: "#",
+        target: "_blank",
+      },
+      {
+        name: "Media & Press",
+        url: "#",
+        target: "_blank",
+      },
+      {
+        name: "FAQ",
+        url: "#",
+        target: "_blank",
+      },
+    ],
+  },
+  legal: {
+    title: "Legal",
+    links: [
+      {
+        name: "Terms of Service",
+        url: "#",
+        target: "_blank",
+      },
+      {
+        name: "Privacy Policy",
+        url: "#",
+        target: "_blank",
+      },
+    ],
+  },
+  copyright: "© Copyright ACCELERATE OR DIE. All rights reserved",
+};
--- app/shared-components/footer.tsx
+++ app/shared-components/footer.tsx
@@ -1,117 +1,101 @@
 import Link from "next/link";
 import { footerCopy } from "../copy/footer";
+import { navbarCopy } from "../copy/navbar";
 
 export default function Footer() {
-    return (
-        <div className="content mx-lg-5">
-            <footer className="container px-0" id="footer">
-                <div className="row white-border bt-half p-5 mb-5 mx-0">
-                    <div className="row ">
-                        <div className="col-lg-4 d-flex flex-column text-center">
-                            <Link href="#" className="mb text-decoration-none">
-                                <h3 className="font-goodtimes text-xlarge text-light">ACCEL</h3>
-                            </Link>
+  return (
+    <div className="content mx-lg-5">
+      <footer className="container px-0" id="footer">
+        <div className="row white-border bt-half p-5 mb-5 mx-0">
+          <div className="row ">
+            <div className="col-lg-4 d-flex flex-column text-center">
+              <Link href="#" className="mb text-decoration-none">
+                <h3 className="font-goodtimes text-xlarge text-light">
+                  {navbarCopy.title}
+                </h3>
+              </Link>
 
-                            <div className="my-2">
-                                <ul className="navbar-nav footer-social list-group-horizontal justify-content-between">
-
-                                    <li className="p-social">
-                                        <Link href={footerCopy["social-links"].x.url}
-                                            target={footerCopy["social-links"].x.target}>
-                                            <img src="./img/icon-x.svg" alt="X" />
-                                        </Link>
-                                    </li>
-                                    <li className="p-social">
-                                        <Link href={footerCopy["social-links"].instagram.url}
-                                            target={footerCopy["social-links"].instagram.target}>
-                                            <img src="./img/icon-instagram.svg" alt="Instagram" />
-                                        </Link>
-                                    </li>
-                                    <li className="p-social"> <Link href={footerCopy["social-links"].github.url} target={footerCopy["social-links"].github.target}>
-                                        <img src="./img/icon-github.svg" alt="Github" />
-                                    </Link>
-                                    </li>
-
-                                    <li className="p-social"> <Link href={footerCopy["social-links"].discord.url} target={footerCopy["social-links"].discord.target}
-                                        data-query="footer.social-links.discord._" data-attribute="href" data-target="target"><img
-                                            src="./img/icon-discord.svg" alt="Discord" /></Link>
-                                    </li>
-                                    <li className="p-social"> <Link href={footerCopy["social-links"].tiktok.url} target={footerCopy["social-links"].tiktok.target}
-                                        data-query="footer.social-links.tiktok._" data-attribute="href" data-target="target"><img
-                                            src="./img/icon-tiktok.svg" alt="TikTok" /></Link>
-                                    </li>
-                                    <li className="p-social"> <Link href={footerCopy["social-links"].youtube.url} target={footerCopy["social-links"].youtube.target}
-                                        data-query="footer.social-links.youtube._" data-attribute="href" data-target="target"><img
-                                            src="./img/icon-youtube.png" alt="Youtube" style={{ width: "26px", height: "36px" }} /></Link>
-                                    </li>
-                                    <li className="p-social"> <Link href={footerCopy["social-links"].linkedin.url} target={footerCopy["social-links"].linkedin.target}
-                                        data-query="footer.social-links.linkedin._" data-attribute="href" data-target="target"><img
-                                            src="./img/icon-linkedin.svg" alt="Linked-in" /></Link>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="my-4 text-thin" data-query="footer.copyright"> <span>© Copyright ACCELERATE OR DIE. All rights
-                                reserved</span>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 ms-auto row mt-lg-0 mt-3 justify-content-end">
-                            <div className="col-lg-4 col-6">
-                                <ul className="list-unstyled">
-                                    <li className="mb-4">
-                                        <h3 className="text-medium" style={{ color: "#57B3E6" }}>Quick Links</h3>
-                                    </li>
-                                    {footerCopy["quick-links"].map(item =>
-                                    (
-                                        <li> <Link href={item.url} target={item.target} className="nav-link text-small text-thin my-2">
-                                            {item.name}
-                                        </Link>
-                                        </li>
-                                    )
-
-                                    )}
-
-                                </ul>
-                            </div>
-                            <div className="col-lg-4 col-6">
-                                <ul className="list-unstyled">
-                                    <li className="mb-4">
-                                        <h3 className="text-medium" style={{ color: "#57B3E6" }}>Resources</h3>
-                                    </li>
-                                    {footerCopy.resources.map(item =>
-                                    (
-                                        <li> <Link href={item.url} target={item.target} className="nav-link text-small text-thin my-2">
-                                            {item.name}
-                                        </Link>
-                                        </li>
-                                    )
-
-                                    )}
-                                </ul>
-                            </div>
-                            <div className="col-lg-4 col-6">
-                                <ul className="list-unstyled">
-                                    <li className="mb-4">
-                                        <h3 className="text-medium" style={{ color: "#57B3E6" }}>Legal</h3>
-                                    </li>
-                                    {footerCopy.legal.map(item =>
-                                    (
-                                        <li> <Link href={item.url} target={item.target} className="nav-link text-small text-thin my-2">
-                                            {item.name}
-                                        </Link>
-                                        </li>
-                                    )
-
-                                    )}
-
-                                </ul>
-                            </div>
-
-                        </div>
-                    </div>
-
-
-                </div>
-            </footer>
+              <div className="my-2">
+                <ul className="navbar-nav footer-social list-group-horizontal justify-content-between">
+                  {footerCopy.socialLinks.map((link, index) => (
+                    <li className="p-social">
+                      <Link href={link.url} target={link.target}>
+                        <img src={link.image} alt={link.name} />
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="my-4 text-thin">
+                <span>{footerCopy.copyright}</span>
+              </div>
+            </div>
+            <div className="col-lg-6 ms-auto row mt-lg-0 mt-3 justify-content-end">
+              <div className="col-lg-4 col-6">
+                <ul className="list-unstyled">
+                  <li className="mb-4">
+                    <h3 className="text-medium" style={{ color: "#57B3E6" }}>
+                      {footerCopy.quickLinks.title}
+                    </h3>
+                  </li>
+                  {footerCopy.quickLinks.links.map((item) => (
+                    <li>
+                      <Link
+                        href={item.url}
+                        target={item.target}
+                        className="nav-link text-small text-thin my-2"
+                      >
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="col-lg-4 col-6">
+                <ul className="list-unstyled">
+                  <li className="mb-4">
+                    <h3 className="text-medium" style={{ color: "#57B3E6" }}>
+                      {footerCopy.resources.title}
+                    </h3>
+                  </li>
+                  {footerCopy.resources.links.map((item) => (
+                    <li>
+                      <Link
+                        href={item.url}
+                        target={item.target}
+                        className="nav-link text-small text-thin my-2"
+                      >
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="col-lg-4 col-6">
+                <ul className="list-unstyled">
+                  <li className="mb-4">
+                    <h3 className="text-medium" style={{ color: "#57B3E6" }}>
+                      {footerCopy.legal.title}
+                    </h3>
+                  </li>
+                  {footerCopy.legal.links.map((item) => (
+                    <li>
+                      {" "}
+                      <Link
+                        href={item.url}
+                        target={item.target}
+                        className="nav-link text-small text-thin my-2"
+                      >
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </footer>
+    </div>
+  );
 }
