e4ee196779f72dc83aa325c52cca6e0d971e4263
Ehsan-saradar <Ehsan.Saradar@gmail.com>
2024-08-08 11:41:50 +0330

    Adjust the spacing of the 'my-journey' items

--- app/components/my-journey/index.tsx
+++ app/components/my-journey/index.tsx
@@ -6,7 +6,7 @@ import { myJournyCopy } from "@/app/copy/my-journey"
 export default function Sections() {
     return (
         <>
-           <div className="row position-relative overflow-hidden">
+            <div className="row position-relative overflow-hidden">
                 <div className="col-md-3">
                     <div className="row">
                         <div className="col-12 white-border br-half bb-half p-0 overflow-hidden">
@@ -35,69 +35,79 @@ export default function Sections() {
             <div className="row white-border bt-half bb-half position-relative overflow-hidden">
                 <div className="p-5 m-0">
                     <h4 className="font-goodtimes text-large text-narrow-height text-center mt-high"
-                        data-query="my-journey.segment-1.title" dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].title}}></h4>
+                        data-query="my-journey.segment-1.title" dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].title }}></h4>
                 </div>
                 <div className="row justify-content-center">
 
-                    <div className="col-md-1"> <img src="./img/journey-timeline.svg" alt="Timeline" /></div>
+                    <div className="col-md-1 TimeLineContainer">
+                        <div className="myJourneyTimeLine"></div>
+                    </div>
                     <div className="col-md-6 ps-4 mb-high">
                         {/* <!-- 1 --> */}
-                        <div style={{height:"280px"}}>
-                            <h4 className="font-goodtimes text-medium" data-query="my-journey.segment-1.items.0.title" 
+                        <div className="myJourneyItems">
+                            <img className="myJourneyicon" src={myJournyCopy["segment-1"].items[0].img} />
+                            <h4 className="font-goodtimes text-medium" data-query="my-journey.segment-1.items.0.title"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[0].title }}></h4>
                             <p className="text-small text-n300"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[0].description }}
-                            data-query="my-journey.segment-1.items.0.description"></p>
+                                data-query="my-journey.segment-1.items.0.description"></p>
                         </div>
                         {/* <!-- 2 --> */}
-                        <div style={{ height: "210px" }}>
+                        <div className="myJourneyItems">
+                            <img className="myJourneyicon" src={myJournyCopy["segment-1"].items[1].img} />
                             <h4 className="font-goodtimes text-medium" data-query="my-journey.segment-1.items.1.title"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[1].title }}></h4>
-                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.1.description" 
+                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.1.description"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[1].description }}></p>
                         </div>
                         {/* <!-- 3 --> */}
-                        <div>
-                            <h4 className="font-goodtimes text-medium" data-query="my-journey.segment-1.items.2.title" 
+                        <div className="myJourneyItems">
+                            <img className="myJourneyicon" src={myJournyCopy["segment-1"].items[2].img} />
+                            <h4 className="font-goodtimes text-medium" data-query="my-journey.segment-1.items.2.title"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[2].title }}></h4>
-                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.2.description" 
+                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.2.description"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[2].description }}></p>
                         </div>
                         {/* <!-- 4 --> */}
-                        <div>
-                            <h4 className="font-goodtimes text-medium mt-5" data-query="my-journey.segment-1.items.3.title" 
+                        <div className="myJourneyItems">
+                            <img className="myJourneyicon" src={myJournyCopy["segment-1"].items[3].img}/>
+                            <h4 className="font-goodtimes text-medium mt-5" data-query="my-journey.segment-1.items.3.title"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[3].title }}></h4>
-                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.3.description" 
+                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.3.description"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[3].description }}></p>
                         </div>
                         {/* <!-- 5 --> */}
-                        <div>
-                            <h4 className="font-goodtimes text-medium mt-4" data-query="my-journey.segment-1.items.4.title" 
+                        <div className="myJourneyItems">
+                            <img className="myJourneyicon" src={myJournyCopy["segment-1"].items[4].img}/>
+                            <h4 className="font-goodtimes text-medium mt-4" data-query="my-journey.segment-1.items.4.title"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[4].title }}></h4>
                             <p className="text-small text-n300" data-query="my-journey.segment-1.items.4.description"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[4].description }}></p>
                         </div>
                         {/* <!-- 6 --> */}
-                        <div>
-                            <h4 className="font-goodtimes text-medium mt-4" 
+                        <div className="myJourneyItems">
+                            <img className="myJourneyicon" src={myJournyCopy["segment-1"].items[5].img} />
+                            <h4 className="font-goodtimes text-medium mt-4"
                                 data-query="my-journey.segment-1.items.5.title"
-                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[5].title }}></h4>
-                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.5.description" 
+                                dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[5].title }}></h4>
+                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.5.description"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[5].description }}></p>
                         </div>
                         {/* <!-- 7 --> */}
-                        <div>
-                            <h4 className="font-goodtimes text-medium mt-5" data-query="my-journey.segment-1.items.6.title" 
+                        <div className="myJourneyItems">
+                            <img className="myJourneyicon" src={myJournyCopy["segment-1"].items[6].img} />
+                            <h4 className="font-goodtimes text-medium mt-5" data-query="my-journey.segment-1.items.6.title"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[6].title }}></h4>
-                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.6.description" 
+                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.6.description"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[6].description }}></p>
                         </div>
                         {/* <!-- 8 --> */}
-                        <div>
-                            <h4 className="font-goodtimes text-medium mt-4" 
-                                data-query="my-journey.segment-1.items.7.title" 
+                        <div className="myJourneyItems">
+                            <img className="myJourneyicon" src={myJournyCopy["segment-1"].items[7].img}/>
+                            <h4 className="font-goodtimes text-medium mt-4"
+                                data-query="my-journey.segment-1.items.7.title"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[7].title }}></h4>
-                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.7.description" 
+                            <p className="text-small text-n300" data-query="my-journey.segment-1.items.7.description"
                                 dangerouslySetInnerHTML={{ __html: myJournyCopy["segment-1"].items[7].description }}></p>
                         </div>
                     </div>
